9a180a38223b5866544281ec3818440df5e415d2
rhyanndev <rhyannjosebezerra@gmail.com>
2025-11-08 10:43:40 -0300

    feat: Alteração de nome rota de criar comunidades e título da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import AderirComunidade from "./pages/AderirComunidade/AderirComunidade";
+import CriarComunidade from "./pages/Comunidade/CriarComunidade";
 import Comunidades from "./pages/Comunidade/Comunidade";
 import Login from "./pages/Participante/Login";
 import Register from "./pages/Participante/Register";
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/criar" element={<CriarComunidade />} />
         <Route path="/aderir" element={<AderirComunidade />} />
+        <Route path="/criar-comunidade" element={<CriarComunidade />} />
         <Route path="/comunidade/:id" element={<Comunidades />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
